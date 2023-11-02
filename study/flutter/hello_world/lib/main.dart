80af15fd48ffdc7c6bad6afe7751bc6616b0985e
KimLogan0520 <kimlogan0520@gmail.com>
2023-11-02 17:03:45 +0900

    flutter ~132

--- study/flutter/hello_world/lib/main.dart
+++ study/flutter/hello_world/lib/main.dart
@@ -9,8 +9,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Hello World!',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.blue,
+                ),
+              ),
+              TextButton(
+                onPressed: (){},
+                style: TextButton.styleFrom(foregroundColor: Colors.red),
+                child: Text('텍스트 버튼'),
+              ),
+              OutlinedButton(
+                onPressed: (){},
+                style: OutlinedButton.styleFrom(foregroundColor: Colors.red),
+                child: Text('아웃라인드 버튼'),
+              ),
+              ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(foregroundColor: Colors.red),
+                child: Text('엘레베이티드 버튼'),
+              )
+            ],
+          ),
         )
       )
     );
