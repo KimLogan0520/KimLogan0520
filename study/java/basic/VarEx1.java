8a0a651df54f7488439455511dc45e5971fc0d88
KimLogan0520 <kimlogan0520@gmail.com>
2023-11-22 01:59:38 +0900

    java js ~p32

--- study/java/basic/VarEx1.java
+++ study/java/basic/VarEx1.java
@@ -1,12 +1,12 @@
-class VarEx1{
+class VarEx1 {
     public static void main(String[] args) {
         int year = 0;
-        int age = 32;
+        int age = 14;
 
         System.out.println(year);
         System.out.println(age);
 
-        year = year + 2023;
+        year = age + 2000;
         age = age + 1;
 
         System.out.println(year);
--- study/java/basic/VarEx2.java
+++ study/java/basic/VarEx2.java
@@ -1,12 +1,14 @@
 class VarEx2 {
     public static void main(String[] args) {
         int x = 10, y = 20;
-        int tmp = 0;
-        System.out.println("x : " + x + ", y : " + y);
+        int temp = 0;
 
-        tmp = x;
+        System.out.println("x :" + x + ", y :" + y);
+
+        temp = x;
         x = y;
-        y = tmp;
-        System.out.println("x : " + x + ", y : " + y);
+        y = temp;
+
+        System.out.println("x :" + x + ", y :" + y);
     }
 }
