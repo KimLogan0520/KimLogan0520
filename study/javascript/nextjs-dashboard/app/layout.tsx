dce6e9c239f981bff377bc00efc61cd6f4a8173d
KimLogan0520 <kimlogan0520@gmail.com>
2023-11-22 17:43:49 +0900

    nextjs tuto The <Link> compo ~

--- study/javascript/nextjs-dashboard/app/layout.tsx
+++ study/javascript/nextjs-dashboard/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from "@/app/ui/fonts";
 
+// 최상단에 위치한 root layout
+// 여기에 추가한 모든 ui요소는 모든 페이지에 적용된다.
 export default function RootLayout({
   children,
 }: {
